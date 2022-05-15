9ec0b9fc879b0ad10b9e742253316ad2d6235cdd
molivaresr <m.olivares.r@gmail.com>
2022-05-15 13:28:22 +0200

    Connecting with online server

--- itchat/src/config/default.js
+++ itchat/src/config/default.js
@@ -1,4 +1,4 @@
-export const SOCKET_URL = process.env.SOCKET_URL || 'http://localhost:4000'; // Servidor
+export const SOCKET_URL = process.env.SOCKET_URL || 'https://itchatserver.herokuapp.com/'; // Servidor
 
-export const api = 'http://localhost:4000/api'
-export const chat = 'http://localhost:4000/api/chat'
+export const api = 'https://itchatserver.herokuapp.com/api'
+export const chat = 'https://itchatserver.herokuapp.com/api/chat'
